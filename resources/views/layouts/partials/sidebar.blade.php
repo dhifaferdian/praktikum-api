89d87475f5c79c658dbdb8fab1ce1fe78a3a8a4a
dhifaferdian <ferdiandifa@gmail.com>
2025-10-23 14:42:06 +0700

    YOlah

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -35,11 +35,12 @@
 
             <!--
             <div class="sidebar-card d-none d-lg-flex">
-                <img class="sidebar-card-illustration mb-2" src="img/undraw_rocket.svg" alt="...">
-                <p class="text-center mb-2"><strong>SB Admin Pro</strong> is packed with premium features, components, and more!</p>
-                <a class="btn btn-success btn-sm" href="https://startbootstrap.com/theme/sb-admin-pro">Upgrade to Pro!</a>
+                <img class="sidebar-card-illustration mb-2" src="{{ asset('img/pragmatic.png') }}"alt="logo">
+                <p class="text-center mb-2"><strong>Pragmatic</strong> WD</p>
+                <a class="btn btn-success btn-sm" href="https://startbootstrap.com/theme/sb-admin-pro">Main Sekarang</a>
             </div>
             -->
+            
             
             
     
